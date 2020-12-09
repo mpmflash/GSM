397cebc5243b6512af5794c898209cc33927a65f
uge02005 <uge02005@SCB-LP-0002.CENTRAL.HOTUSA.INT>
2020-12-09 17:58:34 +0100

    Montando la clase PedidoWindow

--- src/Program/PedidoWindow.java
+++ src/Program/PedidoWindow.java
@@ -1,11 +1,36 @@
 package Program;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class PedidoWindow extends JFrame {
 	// Atributos de la clase
+	// JPanels
+	private JPanel pNPedido;
+	private JPanel pCenter;
+	private JPanel pDatos;
+	private JPanel pMaterial;
+	private JPanel pObservaciones;
+	
+	// Zonas de la ventana:
+	// pNPedido
+	private JTextField tFNPedido;
+	
+	// pDatos
+	private JTextField tFPersona;
+	private JTextField tFDpto;
+	private JTextField tFAutorizado;
+	private JTextField tFTecnico;
+	
+	// pMaterial
+	
+	// pObservaciones
 	
 	// Constructor vacío
 	public PedidoWindow() {
@@ -14,9 +39,56 @@ public class PedidoWindow extends JFrame {
 		this.setSize(400,300); //Tamaño de la ventana
 		this.setLocationRelativeTo(null); // Esto hace que se centre la ventana
 		getContentPane().setLayout(new BorderLayout());
+		this.setVisible(true);
 		// Iniciamos los diferentes paneles en métodos para limpiar el cuerpo de la clase
-		
+		initPanelpNPedido();
+		initPanelpCenter();
+		initPanelpDatos();
 	}
 	// Métodos de la clase
+	/*
+	 * initPanelpNPedido();
+	 * Método para preparar el JPanel pNPedido
+	 * @param void
+	 * @return void
+	 */
+	private void initPanelpNPedido() {
+		pNPedido = new JPanel();
+		pNPedido.setLayout(new FlowLayout());
+		JLabel lblNPedido = new JLabel("Nº Pedido");
+		tFNPedido = new JTextField();
+		tFNPedido.setColumns(15);
+		pNPedido.add(lblNPedido);
+		pNPedido.add(tFNPedido);
+		this.getContentPane().add(pNPedido, BorderLayout.NORTH);
+	}
 	
+	/*
+	 * initPanelpCenter();
+	 * Método para preparar el JPanel pCenter
+	 * @param void
+	 * @return void
+	 */
+	private void initPanelpCenter() {
+		pCenter = new JPanel();
+		pCenter.setLayout(new BorderLayout());
+		this.getContentPane().add(pCenter, BorderLayout.CENTER);
+	}
+	/*
+	 * initPanelpDatos();
+	 * Método para preparar el JPanel pDatos
+	 * @param void
+	 * @return void
+	 */
+	private void initPanelpDatos() {
+		pDatos = new JPanel();
+		pDatos.setLayout(new GridLayout());
+		JLabel lblPersona = new JLabel("Persona");
+		tFPersona = new JTextField();
+		tFPersona.setColumns(10);
+		JLabel lblDpto = new JLabel("Dpto.");
+		tFDpto = new JTextField();
+		tFDpto.setColumns(10);
+		
+	}
 }
